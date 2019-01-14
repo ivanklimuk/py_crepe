c6fa182aec7bc54ad44f2ec2ff01464c280a033c
ivanklimuk <ivanklimuk96@gmail.com>
2019-01-15 00:19:08 +0300

    Apply minor refactoring

--- utils.py
+++ utils.py
@@ -4,47 +4,42 @@ import pandas as pd
 from keras.utils.np_utils import to_categorical
 
 
-def load_ag_data():
-    train = pd.read_csv('data/ag_news_csv/train.csv', header=None)
-    train = train.dropna()
+def load_train_data(path, labels_path=None):
+    '''
+    Load the train dataset with the labels:
+    - either as the second value in each row in the read_csv
+    - or as a separate file
+    '''
+    train_text = np.array(pd.read_csv(path, header=None))
+    if labels_path:
+        train_labels = np.array(pd.read_csv(labels_path, header=None))
+    else:
+        train_text, train_labels = train_text[:, 0], train_text[:, 1]
+    train_labels = to_categorical(train_labels)
 
-    x_train = train[1] + train[2]
-    x_train = np.array(x_train)
-
-    y_train = train[0] - 1
-    y_train = to_categorical(y_train)
-
-    test = pd.read_csv('data/ag_news_csv/test.csv', header=None)
-    x_test = test[1] + test[2]
-    x_test = np.array(x_test)
-
-    y_test = test[0] - 1
-    y_test = to_categorical(y_test)
-
-    return (x_train, y_train), (x_test, y_test)
+    return (train_text, train_labels)
 
 
-def encode_data(x, maxlen, vocab):
-    # Iterate over the loaded data and create a matrix of size (len(x), maxlen)
-    # Each character is encoded into a one-hot array later at the lambda layer.
-    # Chars not in the vocab are encoded as -1, into an all zero vector.
+def text_to_array(text, max_len, vocabulary):
+    '''
+    Iterate over the loaded text and create a matrix of size (len(text), max_len)
+    Each character is encoded into a one-hot array later at the lambda layer.
+    Chars not in the vocab are encoded as -1, into an all zero vector.
+    '''
 
-    input_data = np.zeros((len(x), maxlen), dtype=np.int)
-    for dix, sent in enumerate(x):
-        counter = 0
-        for c in sent:
-            if counter >= maxlen:
-                pass
-            else:
-                ix = vocab.get(c, -1)  # get index from vocab dictionary, if not in vocab, return -1
-                input_data[dix, counter] = ix
-                counter += 1
-    return input_data
+    text_array = np.zeros((len(text), max_len), dtype=np.int)
+    for row, line in enumerate(text):
+        for column in range(min([len(line), max_len])):
+            text_array[row, column] = vocabulary.get(line[column], -1)  # if not in vocabulary_size, return -1
+
+    return text_array
 
 
 def create_vocabulary_set(ascii=True, digits=True, punctuation=True):
-    # This alphabet is 69 chars vs. 70 reported in the paper since they include two
-    # '-' characters. See https://github.com/zhangxiangxiao/Crepe#issues.
+    '''
+    This alphabet is 69 chars vs. 70 reported in the paper since they include two
+    '-' characters. See https://github.com/zhangxiangxiao/Crepe#issues.
+    '''
 
     alphabet = []
     if ascii:
